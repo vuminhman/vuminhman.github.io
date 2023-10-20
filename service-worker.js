3ede51a55b0ab07e302cdb516dacf8fd580eab96
manvu <man.vu@mohawkcollege.ca>
2023-10-20 00:52:47 -0400

    redeployed and testing

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9691adb2cd8e772393877493714fcae9.js"
+  "/precache-manifest.16e0966344279be0e06a675b4bfc1083.js"
 );
 
 self.addEventListener('message', (event) => {
